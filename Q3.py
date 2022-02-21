38d4d26505d685c91dc74878ddf1fd0f31bbb4de
valtemirprocopio <96804350+valtemirprocopio@users.noreply.github.com>
2022-02-20 22:49:54 -0300

    20/02/20222
    Escreva um programa leia dois nomes de times de futebol, a quantidade de gols do primeiro time,
    a quantidade de gols do segundo time e mostre o nome do time vencedor da partida.

---
+++ Q3.py
@@ -0,0 +1,16 @@
+'''Escreva um programa leia dois nomes de times de futebol, a quantidade de gols do primeiro time, 
+a quantidade de gols do segundo time e mostre o nome do time vencedor da partida.'''
+
+import string
+
+
+first_team = input('Enter the name of the first team: ')
+first_team_goals = int(input('Enter the number of goals of the team ' + first_team + ' :'))
+
+second_team = input('Enter the name of the second team: ')
+second_team_goals = int(input('Enter the number of goals of the team ' + second_team + ' :'))
+
+if (first_team_goals > second_team_goals):
+    print ('The winning team is ' + first_team)
+else:
+    print ('The winning team is ' + second_team)
