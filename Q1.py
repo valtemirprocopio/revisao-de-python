43adbab93f307656d4bf79965164218f6a1bfe28
valtemirprocopio <96804350+valtemirprocopio@users.noreply.github.com>
2022-02-20 22:29:32 -0300

    Update

--- Q1.py
+++ Q1.py
@@ -1,3 +1,9 @@
-# 1. Escreva um programa que leia um número e 
-# informe se ele é positivo ou negativo.
+''' 1. Escreva um programa que leia um número e 
+ informe se ele é positivo ou negativo.'''
+
+number = int(input('Enter a number: '))
+if number < 0:
+    print ('The number is negative.')
+else:
+    print ('The number is positive.')
 
---
+++ Q2.py
@@ -0,0 +1,12 @@
+'''Faça um algoritmo que leia 3 números inteiros 
+distintos e escreva o menor deles.'''
+
+first = int(input('Enter a first number: '))
+second = int(input('Enter a second number: '))
+third = int(input('Enter a third number: '))
+if ((first > second) and (first > third)):
+    print ('The biggest number is', first)
+if ((second > first) and (second > third)):
+    print ('The biggest number is', second)
+if ((third > second) and (third > first)):
+    print ('The biggest number is', third)
