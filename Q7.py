0cfcafa3cffa2fa338ac7ff7124eb8fd3fc22c45
valtemirprocopio <96804350+valtemirprocopio@users.noreply.github.com>
2022-02-22 00:54:04 -0300

    Update Q7

--- Q7.py
+++ Q7.py
@@ -1,1 +1,10 @@
-
+'''Escreva um programa que leia um número inteiro n e mostre todos 
+os números entre 0 e n.'''
+n = int(input('Enter the number:'))
+control=0
+if n < 0:
+    lista = sorted(list(range(n+1,0,1)), reverse= True)
+    print(f'Numbers between 0 e {n}: {lista}')
+else:
+    lista = list(range(1,n,1))
+    print(f'Numbers between 0 e {n}: {lista}')
