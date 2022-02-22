28c84ef74e0e81a2122494285213652f08acfdc3
valtemirprocopio <96804350+valtemirprocopio@users.noreply.github.com>
2022-02-22 01:16:59 -0300

    Update Q8.py

--- Q8.py
+++ Q8.py
@@ -4,7 +4,7 @@ são múltiplos de 3 e 7.'''
 number=1
 print('Multiples of 3 and 7 between 1 and 100:')
 while number < 100:
-    number = number +1
+    number+=1
     if (number % 3 == 0 and number % 7 == 0):
         print(number)
 
