e93109c6ce4c0143f09edbfa491d0fb05357cf53
valtemirprocopio <96804350+valtemirprocopio@users.noreply.github.com>
2022-02-22 01:15:19 -0300

    Update Q8

--- Q8.py
+++ Q8.py
@@ -1,7 +1,8 @@
 '''Escreva um programa que mostre todos os números entre 1 e 100 que 
 são múltiplos de 3 e 7.'''
 
-number=0
+number=1
+print('Multiples of 3 and 7 between 1 and 100:')
 while number < 100:
     number = number +1
     if (number % 3 == 0 and number % 7 == 0):
